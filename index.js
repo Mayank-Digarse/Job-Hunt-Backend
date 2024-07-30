72ac2926c2ca586e4d5676d6f5793a009b984cbc
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2024-07-31 01:40:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'*',
-    credentials:true
-}
-
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin:'*',
+//     credentials:true
+// }
+app.use(cors());
+// app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
